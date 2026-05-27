e5a563859883cac5eb21f1e0b5d68e9c60600fbc
Jagan Prasad <jaganprasad093@gmail.com>
2026-05-27 15:14:48 +0530

    updated minor changes in axios

--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -15,7 +15,6 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = getToken();
-  // console.log("token ------- ", token)
   if (token) {
     config.headers.accesstoken = token;
   }
