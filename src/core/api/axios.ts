035710a3ee7a1fbc39d11111737000b62b6bba7d
Jagan Prasad <jaganprasad093@gmail.com>
2026-05-29 12:11:05 +0530

    chore: update API_BASE_URL to point to the live server environment

--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken, clearToken } from "@/core/utils/auth";
 
 // export const API_BASE_URL = "http://192.168.0.201:3000"; //Nibin
-export const API_BASE_URL = "http://localhost:3000/";  //Local host
-// export const API_BASE_URL = "https://api.anad.ae/";  //Live server
+// export const API_BASE_URL = "http://localhost:3000/";  //Local host
+export const API_BASE_URL = "https://api.anad.ae/";  //Live server
 
 
 export const api = axios.create({
